3f9dd822529bf1788c730d8a93875a9fecc4c74e
Danna <danna_3041240405@utd.edu.mx>
2026-04-24 15:05:00 -0600

    Actualizacion final
    fix: merge changes

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ function verificarSesion(req, res, next) {
   if (req.session && req.session.user) {
     next();
   } else {
-    console.log("Acceso bloqueado");
+    console.log("Access blocked");
     res.redirect("/");
   }
 }
@@ -60,68 +60,51 @@ app.listen(3000, () => {
 
 //LOGIN 
 app.get("/", (req, res) => {
-  // Si ya hay sesión, no mostrar login
   if (req.session.user) {
     return res.redirect("/dashboard");
   }
   res.sendFile(__dirname + "/public/login.html");
 });
-
 //REGISTRO
 app.get("/register", (req, res) => {
   res.sendFile(__dirname + "/public/register.html");
 });
 
-
 //DASHBOARD
 app.get("/dashboard", verificarSesion, (req, res) => {
   res.sendFile(__dirname + "/private/dashboard.html");
 });
-
-
 //VENTAS
 app.get("/ventas", verificarSesion, (req, res) => {
   res.sendFile(__dirname + "/private/ventas.html");
 });
-
-
 //INVENTORY
 app.get("/inventory", verificarSesion, (req, res) => {
   res.sendFile(__dirname + "/private/inventory.html");
 });
-
 //HISTORIAL DE VENTAS DIARIO
 app.get("/history", verificarSesion, (req, res) => {
-
-res.sendFile(__dirname + "/private/history.html");
-
+  res.sendFile(__dirname + "/private/history.html");
 });
+
 
 //LOGIN
 app.post("/login", (req, res) => {
-
   const { name, password } = req.body;
 
   db.query(
     "SELECT * FROM users WHERE name = ? AND password = ?",
     [name, password],
     (err, result) => {
-
       if (err) {
         console.error(err);
-        return res.send("Error en el servidor");
+        return res.send("Server Error");
       }
-
       if (result.length > 0) {
-
         req.session.user = result[0];
-
         res.redirect("/dashboard");
-
       } else {
-
         res.redirect("/?error=1");
-
       }
 
     }
@@ -132,27 +115,22 @@ app.post("/login", (req, res) => {
 
 //LOGOUT
 app.get("/logout", (req, res) => {
-
   req.session.destroy(() => {
-
     res.redirect("/");
-
   });
-
 });
 
 //REGISTRO
 app.post("/register", (req, res) => {
 
   const { name, password } = req.body;
-
   db.query(
     "INSERT INTO users (name, password) VALUES (?, ?)",
     [name, password],
     (err) => {
       if (err) {
         console.error(err);
-        return res.send("Error al registrar usuario");
+        return res.send("Error registering user");
       }
       res.redirect("/");
 
@@ -168,27 +146,23 @@ app.post("/vender", verificarSesion, (req, res) => {
     "SELECT price, stock FROM albums WHERE id = ?",
     [album_id],
     (err, result) => {
-
       if (err) return res.send("Error");
-
       if (result.length === 0)
         return res.send("Album no encontrado");
-
       const album = result[0];
       if (album.stock < cantidad) {
-        return res.send("No hay suficiente stock ❌");
+        return res.send("Not enough stock");
       }
       const total = album.price * cantidad;
       db.query("INSERT INTO ventas (album_id, cantidad, total) VALUES (?, ?, ?)",
         [album_id, cantidad, total],
         (err) => {
-
-          if (err) return res.send("Error al vender");
+          if (err) return res.send("Error when selling album");
           db.query("UPDATE albums SET stock = stock - ? WHERE id = ?",
             [cantidad, album_id],
             (err) => {
-              if (err) return res.send("Error al actualizar stock");
-                res.send("Venta realizada ✅");
+              if (err) return res.send("Error when updating stock");
+                res.send("Sale completed✅");
             }
           );
 
@@ -202,17 +176,29 @@ app.post("/vender", verificarSesion, (req, res) => {
 
 
 //BUSCAR
-app.get("/buscar", verificarSesion, (req, res) => {
-  const nombre = req.query.nombre;
-  const query = `SELECT * FROM albums WHERE tittle LIKE ?`;
-  db.query(query, [`%${nombre}%`], (err, results) => {
-    if (err) throw err;
-    res.json(results);
-  });
+// BUSCAR EN VENTAS
+app.get("/buscar-ventas", verificarSesion, (req, res) => {
+
+const nombre = req.query.nombre;
+
+const query = `
+SELECT * FROM albums 
+WHERE tittle LIKE ? 
+AND activo = TRUE
+`;
+
+db.query(query, [`%${nombre}%`], (err, results) => {
+
+if (err) {
+ console.error(err);
+ return res.send("Error when searching");
+}
+
+res.json(results);
 
 });
 
-
+});
 //FINALIZAR VENTA
 app.post("/finalizar-venta", verificarSesion, (req, res) => {
   const { carrito, total } = req.body;
@@ -237,74 +223,104 @@ app.post("/finalizar-venta", verificarSesion, (req, res) => {
 });
 
 //INVENTORY
-// OBTENER ALBUMS
+
+// OBTENER ALBUMS 
 app.get("/albums", verificarSesion, (req, res) => {
 
 db.query(
-`SELECT albums.*, genre.name AS genero
-FROM albums
-JOIN genre ON albums.id_genre = genre.id`,
+`SELECT albums.*, genre.name AS genero 
+ FROM albums 
+ JOIN genre ON albums.id_genre = genre.id 
+ WHERE albums.activo = TRUE`,
 (err, result) => {
 
 if (err) {
-console.error(err);
-return res.send("Error al obtener albums");
+ console.error(err);
+ return res.send("Error retrieving albums");
 }
 
 res.json(result);
 
 }
-
 );
 
+});
+// ELIMINAR 
+app.delete("/albums/:id", verificarSesion, (req, res) => {
+
+const id = req.params.id;
+
+db.query("UPDATE albums SET activo = FALSE WHERE id = ?",[id],(err, result) => {
+if (err) {
+ console.error(err);
+ return res.send("Error deleting album");
+}
+res.send("Deleted album");
+}
+);
 });
 
 // AGREGAR
 app.post("/albums", verificarSesion, (req, res) => {
-
 const { title, artist, id_genre, anio, price, stock } = req.body;
-
 db.query(
-"INSERT INTO albums (tittle, artist, id_genre, anio, price, stock) VALUES (?, ?, ?, ?, ?, ?)",
-[title, artist, id_genre, anio, price, stock],
-(err) => {
+  "INSERT INTO albums (tittle, artist, id_genre, anio, price, stock) VALUES (?, ?, ?, ?, ?, ?)",
+  [title, artist, id_genre, anio, price, stock],
+  (err) => {
 
 if (err) {
-console.error(err);
-return res.send("Error al agregar album");
+  console.error(err);
+  return res.send("Error when adding album");
 }
-
-res.send("Album agregado");
-
+res.send("Album added");
 }
-
 );
+});
+
+//Buscar
+// BUSCAR
+app.get("/buscar", verificarSesion, (req, res) => {
+
+const nombre = req.query.nombre;
+
+const query = `
+SELECT 
+  albums.*, 
+  genre.name AS genero
+FROM albums
+JOIN genre 
+ON albums.id_genre = genre.id
+WHERE albums.tittle LIKE ?
+AND albums.activo = TRUE
+`;
+
+db.query(query, [`%${nombre}%`], (err, results) => {
+
+if (err) {
+ console.error(err);
+ return res.send("Error when searching");
+}
+
+res.json(results);
+
+});
 
 });
 // EDITAR
 app.put("/albums/:id", verificarSesion, (req, res) => {
-
 const id = req.params.id;
-
 const { title, artist, id_genre, anio, price, stock } = req.body;
-
 db.query(
 "UPDATE albums SET tittle=?, artist=?, id_genre=?, anio=?, price=?, stock=? WHERE id=?",
 [title, artist, id_genre, anio, price, stock, id],
-
 (err) => {
-
 if (err) {
-console.error(err);
-return res.send("Error al actualizar album");
+  console.error(err);
+  return res.send("Error when updating album");
 }
-
-res.send("Album actualizado");
-
+res.send("Album updated");
 }
-
 );
-
 });
 
 
@@ -335,7 +351,7 @@ ORDER BY ventas.fecha DESC`,
 
 if (err) {
 console.error(err);
-return res.send("Error al obtener ventas del día");
+return res.send("Error obtaining sales of the day");
 }
 
 res.json(result);
@@ -368,7 +384,7 @@ ORDER BY ventas.fecha DESC`,
 
 if (err) {
 console.error(err);
-return res.send("Error al obtener historial");
+return res.send("Error obtaining sales history");
 }
 
 res.json(result);
@@ -390,7 +406,7 @@ WHERE DATE(fecha) = CURDATE()`,
 
 if (err) {
 console.error(err);
-return res.send("Error al obtener total del día");
+return res.send("Error obtaining total of the day");
 }
 
 res.json(result[0]);
